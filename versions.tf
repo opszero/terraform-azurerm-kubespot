31b1d1ce74053d48d0f58464da172ca071d7a813
Sohan Yadav <31764946+sohanyadav@users.noreply.github.com>
2025-10-09 13:50:46 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.30.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.17.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.37.1"
-    }
 
-  }
-  required_version = ">= 0.13"
-}
